4591720f4a461d323d9c6445899f6d12e4009fa8
mew <chinchillazz@gmail.com>
2016-05-28 20:55:25 +0800

    added video feature

--- src/components/movie-item.js
+++ src/components/movie-item.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import { Thumbnail } from 'react-bootstrap';
+import { Panel } from 'react-bootstrap';
 
 export default class MovieItem extends Component {
+
   render () {
     return (
       <div className="swiper-slide">
-      <Thumbnail src={this.props.image}>
-        <h3>{this.props.title}</h3>
-      </Thumbnail>
+        <Panel onClick={this._handleClick.bind(this)}>
+          <img src={this.props.images[0].url}/>
+          <h3>{this.props.title}</h3>
+        </Panel>
       </div>
     );
+  }
+
+  _handleClick(event){
+    this.props.clicked(this.props.contents[0].url);
   }
 }
--- src/components/movies-list.js
+++ src/components/movies-list.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import MovieItem from './movie-item.js';
 import request from 'superagent';
 import Swiper from 'swiper';
+import Video from 'react-videojs';
 
 export default class MoviesList extends Component {
 
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
-      movies: []
+      movies: [],
+      videoUrl: ""
     };
   }
 
@@ -23,44 +25,48 @@ export default class MoviesList extends Component {
 
   render() {
     const movieItems = this.state.movies.map((movie, i) => {
-      return (<MovieItem title={movie.title} description={movie.description} image={movie.images[0].url} key={i}/>)
-    });
-    if(movieItems.length > 0){
-      setTimeout(function() {
-        new Swiper ('.swiper-container', {
-      direction: 'horizontal',
-      scrollbar: '.swiper-scrollbar',
-      scrollbarHide: true,
-      scrollbarDraggable: true,
-      slidesPerView: 4,
-      paginationClickable: true,
-      spaceBetween: 30,
-      mousewheelControl: true,
-      keyboardControl: true,
-      grabCursor: true,
-      breakpoints: {
-            1024: {
+      return (<MovieItem
+        { ...movie }
+        key={i}
+        clicked={this._onClick.bind(this)}/>)
+      });
+
+      if(movieItems.length > 0){
+        setTimeout(function() {
+          new Swiper ('.swiper-container', {
+            direction: 'horizontal',
+            scrollbar: '.swiper-scrollbar',
+            scrollbarHide: true,
+            scrollbarDraggable: true,
+            slidesPerView: 4,
+            paginationClickable: true,
+            spaceBetween: 30,
+            mousewheelControl: true,
+            keyboardControl: true,
+            grabCursor: true,
+            breakpoints: {
+              1024: {
                 slidesPerView: 4,
                 spaceBetween: 40
-            },
-            768: {
+              },
+              768: {
                 slidesPerView: 3,
                 spaceBetween: 30
-            },
-            640: {
+              },
+              640: {
                 slidesPerView: 2,
                 spaceBetween: 20
-            },
-            320: {
+              },
+              320: {
                 slidesPerView: 1,
                 spaceBetween: 10
-            }
-          },
-      nextButton: '.swiper-button-next',
-      prevButton: '.swiper-button-prev'
-      });
-      }, 0);
-    }
+              }
+            },
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev'
+          });
+        }, 0);
+      }
 
     return (
       <div className="swiper-container">
@@ -70,7 +76,24 @@ export default class MoviesList extends Component {
         <div className="swiper-scrollbar"></div>
         <div className="swiper-button-next"></div>
         <div className="swiper-button-prev"></div>
+        { this._playVideo() }
       </div>
     );
+  }
+
+  _playVideo() {
+    if(this.state.videoUrl){
+      return (
+        <Video
+          src={ this.state.videoUrl }
+          type="video/mp4"
+          onPlay={this.handlePlay}
+        />
+      );
+    }
+  }
+
+  _onClick(videoUrl){
+    this.setState({videoUrl: videoUrl});
   }
 }
